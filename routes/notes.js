59c00853571cad71b338c229614c6036faaf4c47
john <johnjacobcosgrove@gmail.com>
2018-08-14 16:24:35 -0400

    updated endpoints

--- routes/notes.js
+++ routes/notes.js
@@ -1,50 +1,135 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('../config');
+
+const Note = require('../models/note');
 
 const router = express.Router();
 
 /* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
 
-  console.log('Get All Notes');
-  res.json([
-    { id: 1, title: 'Temp 1' },
-    { id: 2, title: 'Temp 2' },
-    { id: 3, title: 'Temp 3' }
-  ]);
-
+  mongoose.connect(MONGODB_URI)
+  .then(() => {
+      const { searchTerm } = req.query
+    if(searchTerm){
+      return Note.find()
+      .or([{title:{$regex: searchTerm, $options: 'i'}},{content:{$regex: searchTerm, $options: 'i'}}])
+  }
+  return Note.find()
+})
+    
+  .then(results => {
+      res.json(results);
+  })
+  .then(() => {
+      return mongoose.disconnect()
+  })
+  .catch(err => {
+      console.error(`ERROR: ${err.message}`);
+      next(err);
+  });
+  
 });
 
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/:id', (req, res, next) => {
+  mongoose.connect(MONGODB_URI)
+    .then(()=>{
+        const id = req.params.id
 
-  console.log('Get a Note');
-  res.json({ id: 1, title: 'Temp 1' });
-
+        return Note.findById(id)
+    })
+    .then(result =>{
+        res.json(result)
+    })
+    .then(() => {
+        return mongoose.disconnect()
+    })
+    .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    next(err);
+    })
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
 
-  console.log('Create a Note');
-  res.location('path/to/new/document').status(201).json({ id: 2, title: 'Temp 2' });
+
+  mongoose.connect(MONGODB_URI)
+  .then(()=>{
+      const {title, content} = req.body
+
+      const newItem = {
+        title: title,
+        content: content
+      }
+
+      return Note.create(newItem)
+  })
+  .then(result =>{
+      res.location(req.originalUrl/req.id).status(201).json(result)
+  })
+  .then(()=>{
+      return mongoose.disconnect()
+  })
+.catch(err => {
+console.error(`ERROR: ${err.message}`);
+console.error(err);
+});
 
 });
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/:id', (req, res, next) => {
 
-  console.log('Update a Note');
-  res.json({ id: 1, title: 'Updated Temp 1' });
+  mongoose.connect(MONGODB_URI)
+  .then(()=>{
+  const id =req.params.id
 
+  const {title, content} =req.body
+  const updateObj = {title:title, content:content}
+
+      return Note.findByIdAndUpdate(id,updateObj,{new: true})
+  })
+  .then(result =>{
+    if(result){
+      res.json(result)
+    }
+    else{
+      next()
+    }
+  })   
+  .then(()=>{
+      return mongoose.disconnect()
+  })
+.catch(err => {
+console.error(`ERROR: ${err.message}`);
+next(err);
+});
 });
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/:id', (req, res, next) => {
 
-  console.log('Delete a Note');
-  res.status(204).end();
+  mongoose.connect(MONGODB_URI)
+  .then(()=>{
+      const id = req.params.id
+
+      return Note.findByIdAndRemove(id)
+  })
+      .then(() =>{
+        res.sendStatus(204)
+  })   
+  .then(()=>{
+      return mongoose.disconnect()
+  })
+.catch(err => {
+  console.error(`ERROR: ${err.message}`);
+  next(err);
+});
 });
 
 module.exports = router;
